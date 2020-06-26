f424b41395a878bac7cedf7e7bfe5abf3259216d
Alaa-Shaker <eng.alaashaker4@gmail.com>
2020-06-26 22:50:00 +0300

    image

--- controllers/image.js
+++ controllers/image.js
@@ -5,6 +5,7 @@ const app = new Clarifai.App({ apiKey: 'c85c2e9e07624410a16207684934c3a4'});
 
 const imageURLHandlePost = (req,res)=>{
   const {imageURL} = req.body ;
+  console.log(imageURL);
   app.models.predict("a403429f2ddf4b49b307e318f00e528b", imageURL)
   .then(data =>{res.json(data); })
   .catch(err => res.status(400).json("Can not get Boces of Faces"))
